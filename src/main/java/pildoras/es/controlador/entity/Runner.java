08636fa92ae4e4184af799d83d560f84c06f3252
Claudio Cristian Lopez Linco <lopezlin@kyndryl.com>
2023-08-09 11:12:53 -0300

    Se crea entidad Runner

---
+++ src/main/java/pildoras/es/controlador/entity/Runner.java
@@ -0,0 +1,106 @@
+package pildoras.es.controlador.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="runnerstable")
+public class Runner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="nombre")
+    private String nombre;
+
+    @Column(name="apellido")
+    private String apellido;
+
+    @Column(name="edad")
+    private int edad;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="dorsal")
+    private int dorsal;
+
+    @Column(name="tiempo")
+    private String tiempo;
+
+
+/*******************************************************************/
+
+    public Runner() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(String tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    @Override
+    public String toString() {
+        return "Runner{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
+                ", email='" + email + '\'' +
+                ", dorsal=" + dorsal +
+                ", tiempo='" + tiempo + '\'' +
+                '}';
+    }
+}
